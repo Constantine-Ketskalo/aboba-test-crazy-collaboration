677b11bb3ddc7d8306cc76fddb2e77e1b041ab3c
Шкварок Назарій Васильович <106309210+LogSSS@users.noreply.github.com>
2025-01-27 18:28:12 +0200

    Added NazarioClass with property Nazario
    set string "Nazario" for new NazarioClass property Nazario and prints it

--- AbobaChaos/AbobaChaos/Program.cs
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var nazario = new NazarioClass();
+            Console.WriteLine(nazario.Nazario);
+        }
+    }
+
+
+    public class NazarioClass
+    {
+        public string Nazario { get; set; }
+
+        public NazarioClass()
+        {
+            Nazario = "Nazario";
         }
     }
 }
