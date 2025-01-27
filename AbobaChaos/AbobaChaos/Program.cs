022bdd6ac38b1f19a299d00eaf442e0fcab92f05
Constantine Ketskalo <constantine.ketskalo@gmail.com>
2025-01-27 17:53:34 +0200

    Init: created project, added gitignore.

---
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AbobaChaos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
