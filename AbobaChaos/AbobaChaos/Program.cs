bdc98aac90e0920ee136a7ccb82864acd3f85756
Maksym Pitula <m.pitula.netls.com.ua@gmail.com>
2025-01-27 18:15:10 +0200

    Test something

--- AbobaChaos/AbobaChaos/Program.cs
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            int x = 9 + 20;
+            int y = 10 + 20;
+            int z= 10 + 25;
+            Console.WriteLine(x+y);
             Console.WriteLine("Hello, World!");
         }
     }
