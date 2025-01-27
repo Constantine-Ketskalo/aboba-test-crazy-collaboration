ca6334b01e4ebe657e57d23289e1ae2a931df9b2
Constantine Ketskalo <constantine.ketskalo@gmail.com>
2025-01-27 18:34:41 +0200

    Revert "Added NazarioClass with property Nazario"
    This reverts commit 677b11bb3ddc7d8306cc76fddb2e77e1b041ab3c.

--- AbobaChaos/AbobaChaos/Program.cs
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -8,20 +8,6 @@
             Console.WriteLine(x + " asdfasdf?!");
 
             Console.WriteLine("Hello, World!");
-
-            var nazario = new NazarioClass();
-            Console.WriteLine(nazario.Nazario);
-        }
-    }
-
-
-    public class NazarioClass
-    {
-        public string Nazario { get; set; }
-
-        public NazarioClass()
-        {
-            Nazario = "Nazario";
         }
     }
 }
