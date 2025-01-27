64c6686c155407da22996ee00321dee2185d70fa
Oleh Kavetskyi <olehkavetskyi@gmail.com>
2025-01-27 18:15:12 +0200

    Add Elephant.cs

--- AbobaChaos/AbobaChaos/Program.cs
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace AbobaChaos
+﻿using System;
+
+namespace AbobaChaos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Elephant elephant = new Elephant("Dumbo", 10);
+            elephant.DisplayInfo();
             Console.WriteLine("Hello, World!");
         }
     }
