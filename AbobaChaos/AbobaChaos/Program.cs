d601e0d2bf25496030f57a3e47751dc1426a6f5f
Constantine Ketskalo <constantine.ketskalo@gmail.com>
2025-01-27 18:13:22 +0200

    Testing something.

--- AbobaChaos/AbobaChaos/Program.cs
+++ AbobaChaos/AbobaChaos/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            int x = 9 + 20;
+            Console.WriteLine(x + " asdfasdf?!");
+
             Console.WriteLine("Hello, World!");
         }
     }
