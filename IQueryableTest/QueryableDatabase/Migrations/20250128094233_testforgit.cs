1ae9593acaa923c69403975e86607c6588930354
Ostap <ostap11kizyma@gmail.com>
2025-01-28 11:52:14 +0200

    test

---
+++ IQueryableTest/QueryableDatabase/Migrations/20250128094233_testforgit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueryableDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class testforgit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Architect",
+                table: "Buildings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Architect",
+                table: "Buildings");
+        }
+    }
+}
---
+++ IQueryableTest/QueryableDatabase/Models/Building.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueryableDatabase.Models
+{
+    public class Building
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        [ForeignKey(nameof(Address))]
+        public int? AddressId { get; set; }
+
+        public virtual Address? Address { get; set; }
+
+        //[ForeignKey(...)]
+        //public virtual ICollection<Address> AddressList { get; set; }
+
+        public int Floors { get; set; }
+        public int YearBuilt { get; set; }
+        public string Architect { get; set; }
+    }
+}
